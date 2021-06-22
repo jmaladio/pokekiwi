c0cabdd93b7315031437e113b7aa3502004c7b83
Julián Aladio <jaladio@southernhorizon.tech>
2021-06-22 00:51:47 -0300

    feat: add base imgs and typography component

--- src/components/common/Typography.js
+++ src/components/common/Typography.js
@@ -1,3 +1,23 @@
-export function Typography({ text }) {
+import styled from 'styled-components';
+
+export function Typography({ text, variant }) {
+
+  if (variant === "h1") {
+    return (
+      <StyledH1>{text}</StyledH1>
+    )
+  } else if (variant === "h2") {
+    return (
+      <StyledH2>{text}</StyledH2>
+    )
+  }
   return <div>{text}</div>
 }
+
+const StyledH1 = styled.h1`
+
+`
+
+const StyledH2 = styled.h2`
+
+`
